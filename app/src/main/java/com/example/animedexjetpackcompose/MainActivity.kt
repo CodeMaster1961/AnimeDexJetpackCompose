624d7c636b8008d59c5e9f357cdaeba268c65c9c
aynac <omer.aynaci@hva.nl>
2023-11-22 16:44:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/animedexjetpackcompose/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.animedexjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.animedexjetpackcompose.ui.theme.AnimeDexJetpackComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AnimeDexJetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "anime_list_screen") {
+                        composable("anime_list_screen") {
+
+                        }
+                        composable(
+                            "anime_detail_screen/{animeName}",
+                            arguments = listOf(
+                                navArgument("animeName") {
+                                    type = NavType.StringType
+                                }
+                            )
+                        ) {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AnimeDexJetpackComposeTheme {
+
+    }
+}
