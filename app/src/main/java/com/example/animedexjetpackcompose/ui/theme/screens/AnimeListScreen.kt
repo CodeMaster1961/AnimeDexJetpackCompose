baa37a5161d8de476a160f4da128e18ee11e6e5a
aynac <omer.aynaci@hva.nl>
2023-11-23 09:44:50 +0100

    new picture added

--- app/src/main/java/com/example/animedexjetpackcompose/ui/theme/screens/AnimeListScreen.kt
+++ app/src/main/java/com/example/animedexjetpackcompose/ui/theme/screens/AnimeListScreen.kt
@@ -1,7 +1,9 @@
 package com.example.animedexjetpackcompose.ui.theme.screens
 
+import android.graphics.Bitmap
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -20,19 +22,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.ColorUtils
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import androidx.palette.graphics.Palette
 import com.example.animedexjetpackcompose.R
 
 @Composable
@@ -50,6 +59,13 @@ fun AnimeListScreen(
                 modifier = Modifier
                     .fillMaxWidth()
                     .align(CenterHorizontally)
+                    .background(
+                        if (isSystemInDarkTheme()) {
+                            Color.Black
+                        } else {
+                            Color.White
+                        }
+                    )
             )
             Spacer(modifier = Modifier.height(20.dp))
             AnimeSearchBar(hint = "Search...")
@@ -57,6 +73,7 @@ fun AnimeListScreen(
     }
 
 }
+
 
 @Composable
 fun AnimeSearchBar(
